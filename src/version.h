45732a0d77208c20cf025fb3aff0f35958a5b97a
Gene J.B. <sterr@list.ru>
2026-03-09 18:31:56 +0000

    Fix sound distortion when loud sound, changes.txt

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "v1.1.0-32-g3b9ee9a-dirty"
-#define VERSION_SHORT "v1.1.0"  
-#define VERSION_IS_DIRTY "" == "dirty" || echo "clean"
+#define VERSION_STRING "v1.1.0-39-g8cae5a1-dirty"
+#define VERSION_SHORT "v1.1.0-39-g8cae5a1"
+#define VERSION_IS_DIRTY 1
 
 #endif // VERSION_H
