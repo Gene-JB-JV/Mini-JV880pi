e1ce4e16a20f0b1929fe4b9db1e69a8b18b1dded
Gene J.B. <sterr@list.ru>
2026-03-12 22:42:07 +0000

    damned char

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "v1.1.0-56-g99902a8-dirty"
-#define VERSION_SHORT "v1.1.0-56-g99902a8"
+#define VERSION_STRING "v1.1.0-57-g7b7eb73-dirty"
+#define VERSION_SHORT "v1.1.0-57-g7b7eb73"
 #define VERSION_IS_DIRTY 1
 
 #endif // VERSION_H
